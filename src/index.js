901acbdfb53f7ad89d53522b8af731c673e4b3e8
Simon Luppescu <simon.luppescu@gmail.com>
2019-05-25 16:08:03 +0900

    More changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = React.createElement("h1", {className: "hi"}, "Hello World!");
+ReactDOM.render(element, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
