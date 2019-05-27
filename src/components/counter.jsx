4e9fa3a229345d05c556d32cc8e81fc8c3db4221
Simon Luppescu <simon.luppescu@gmail.com>
2019-05-28 00:49:20 +0900

    Tutorial: Added delete button to pass events between components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -18,19 +18,24 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm">
           Increment
         </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={this.props.onDelete}>
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 ";
-    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,18 +4,26 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 }
     ]
   };
 
+  handleDelete = () => {
+    console.log("Deleted lol");
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            onDelete={this.handleDelete}
+          />
         ))}
       </div>
     );
